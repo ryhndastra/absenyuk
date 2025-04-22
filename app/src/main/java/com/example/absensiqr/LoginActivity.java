715b668498b093f3af915cff9aec16c250daf228
ryhndastra <ryhndastra@gmail.com>
2025-04-22 11:51:26 +0700

    first commit

---
+++ app/src/main/java/com/example/absensiqr/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.absensiqr;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private boolean isLoading = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen sp = SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        sp.setKeepOnScreenCondition(()-> isLoading);
+        new android.os.Handler().postDelayed(()->{
+            isLoading = false;
+        },5000);
+    }
+}
