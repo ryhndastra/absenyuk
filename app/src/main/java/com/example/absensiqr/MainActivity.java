28751ec05d221c1a5309ac04d71f3110f46fc6f3
ryhndastra <ryhndastra@gmail.com>
2025-04-24 13:11:15 +0700

    added function for user to logout

--- app/src/main/java/com/example/absensiqr/MainActivity.java
+++ app/src/main/java/com/example/absensiqr/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.absensiqr;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.absensiqr.apihandler.LoginHandler;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(v -> {
+            LoginHandler.Logout(MainActivity.this, new LoginHandler.CallBack() {
+                @Override
+                public void onSuccess(JSONObject data) {
+                    runOnUiThread(() -> {
+                        Toast.makeText(MainActivity.this, "Kamu berhasil logout", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    });
+                }
+
+                @Override
+                public void onError(String message) {
+                    runOnUiThread(() -> {
+                        Toast.makeText(MainActivity.this, "Error : " + message, Toast.LENGTH_LONG).show();
+                    });
+                }
+            });
         });
     }
 }
